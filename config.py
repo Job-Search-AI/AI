6d80c434f0711599e980d94f3ae1c33c6f1229bf
AI-Dev-Lex-Kim <kimeojin1995@gmail.com>
2025-09-11 09:35:53 +0000

    config: USER_INFO 테스트 데이터 파일 추가

---
+++ config.py
@@ -0,0 +1,16 @@
+USER_INFO = {
+    "searchType": "search", # 검색 타입
+    "searchword": "AI+%EC%97%94%EC%A7%80%EB%8B%88%EC%96%B4", # 검색 키워드 (AI 엔지니어)
+    "loc_mcd": "101000", # 지역 코드
+    "edu_min": "6", # 학력 최소
+    "edu_max": "9", # 학력 최대
+    "edu_none": "y", # 학력 무관
+    "company_cd": "0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10", # 기업 규모 코드
+    "exp_cd": "1", # 경력 코드
+    "exp_none": "y", # 경력 무관
+    "panel_type": "", # 패널 타입
+    "search_optional_item": "y", # 선택 검색 항목
+    "search_done": "y", # 검색 완료
+    "panel_count": "y", # 패널 카운트
+    "preview": "y", # 미리보기
+}
