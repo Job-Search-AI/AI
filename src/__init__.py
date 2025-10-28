057a98d50ce675a7ffcacc7414187d8463896a57
AI-Dev-Lex-Kim <kimeojin1995@gmail.com>
2025-10-28 02:54:47 +0000

    fix: url 쿼리 맵, 유사어 사전 디렉토리 경로 변경

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from .embedding import similarity_docs_retrieval
 from .llm import generate_response
 from .parsing.main import parsing_job_info
 from .url_exchaging.bert_crf import predict_crf_bert
+from .url_exchanger.entity_normalizer import normalize_and_validate_entities
 from .utils.model_keeper import keep_loading_job_model
 
-__all__ = ['crawl_job_html_from_saramin', 'similarity_docs_retrieval', 'generate_response', 'parsing_job_info', 'predict_crf_bert', 'keep_loading_job_model']
+__all__ = ['crawl_job_html_from_saramin', 'similarity_docs_retrieval', 'generate_response', 'parsing_job_info', 'predict_crf_bert', 'normalize_and_validate_entities', 'keep_loading_job_model']
